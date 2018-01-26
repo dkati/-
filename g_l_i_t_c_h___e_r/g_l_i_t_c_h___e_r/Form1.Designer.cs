00b79b23ca606f612c2799aec186a69af6913d28
Dimitris Katikaridis <dkatikaridis@gmail.com>
2018-01-27 01:35:03 +0200

    Ｒｅａｄ　ｂｍｐ，ｃｏｎｖｅｒｔ ｔｏ　ｈｅｘ　ａｎｄ　ｅｘｐｏｒｔ　ｂｍｐ゠ゟー韻サイゅ　因

--- g_l_i_t_c_h___e_r/g_l_i_t_c_h___e_r/Form1.Designer.cs
+++ g_l_i_t_c_h___e_r/g_l_i_t_c_h___e_r/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ＰＡＲＡＤＩＳＥ　ヿドヿ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(223, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(449, 399);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Lines:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(71, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "移がぜ ｅ ｘ ｐ ｏ ｒ ｔ　";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(684, 423);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ｍａｋｅ　ａｒｔ　違ドう";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- g_l_i_t_c_h___e_r/g_l_i_t_c_h___e_r/Form1.cs
+++ g_l_i_t_c_h___e_r/g_l_i_t_c_h___e_r/Form1.cs
@@ -7,14 +7,120 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace g_l_i_t_c_h___e_r
 {
     public partial class Form1 : Form
     {
+
+        string choosenFile;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+            button2.Visible = false;
+            richTextBox1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            
+            label2.Text = "Loading the file...";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                choosenFile = openFileDialog1.FileName;
+                DialogResult _dg = MessageBox.Show(
+                "Ａｌｓｏ　ａｐｐｅｎｄ　ｔｏ　ｔｅｘｔｂｏｘ？　ス現ぅ加",
+                "",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+
+                if (_dg == DialogResult.Yes)
+                {
+                    richTextBox1.Visible = true;
+                    using (var file = File.Open(openFileDialog1.FileName, FileMode.Open))
+                    {
+                        int b;
+                        int countBytes = 0;
+                        int countLines = 0;
+                        StringBuilder builtString = new StringBuilder();
+                        while ((b = file.ReadByte()) >= 0)
+                        {
+                            countLines++;
+                            label1.SuspendLayout();
+                            label1.Text = "Lines:" + countLines;
+                            label1.ResumeLayout();
+
+                            string s = b.ToString("X");
+                            if (s.Length < 2)
+                                s = "0" + s;
+                            builtString.Append(s + " ");
+                            countBytes++;
+                            if (countBytes == 15)
+                            {
+                                builtString.AppendLine();
+                                countBytes = 0;
+                            }
+
+
+                        }
+                        richTextBox1.SelectionStart = richTextBox1.TextLength;
+                        richTextBox1.SelectedText = builtString.ToString();
+                        file.Close();
+
+                    }
+                } else
+                {
+                    label1.Visible = false;
+                    richTextBox1.Visible = true;
+                }
+                // richTextBox1.Text = String.Join("", Array.ConvertAll(x, byteValue => byteValue.ToString()));
+
+                label2.Text = "Finished...";
+                button2.Visible = true;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo info = Directory.GetParent(choosenFile);
+            string parent = info.ToString()+@"\exported_1.bmp";
+            ByteArrayToFile(parent);
+        }
+
+        private void ByteArrayToFile(string fileName)
+        {
+            string strPath = Path.GetFullPath(choosenFile);
+            BinaryReader binReader = new BinaryReader(File.Open(strPath, FileMode.Open));
+
+            long lfileLength = binReader.BaseStream.Length; // init the buffer
+            byte[] btFile = new byte[lfileLength]; //create the damn array
+
+            for (long lIdx = 0; lIdx < lfileLength; lIdx++)
+            {
+                btFile[lIdx] = binReader.ReadByte();
+            }
+           // MessageBox.Show("dsds "+fileName + " "+btFile.Length );
+            try
+            {
+                using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                {
+                    fs.Write(btFile, 0, btFile.Length); //append dat shit and create the image
+                  
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(""+ ex);
+            }
+        }
+
+
     }
 }
