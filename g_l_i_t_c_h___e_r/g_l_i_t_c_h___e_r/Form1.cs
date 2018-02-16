ef1055a37ca80b73138bf9588781429942f11a3a
Yannis Menexes <johnmenex@gmail.com>
2018-02-16 14:08:50 +0200

    ＢｅｎｄＩｍａｇｅ　ｆｕｎｃｔｉｏｎ　ｆｏｒ　αεｓｔｈｅｔｉｑｕｅ　ｇｌｉｔｃｈ　苛ス英咽ス

--- g_l_i_t_c_h___e_r/g_l_i_t_c_h___e_r/Form1.cs
+++ g_l_i_t_c_h___e_r/g_l_i_t_c_h___e_r/Form1.cs
@@ -45,13 +45,16 @@ namespace g_l_i_t_c_h___e_r
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Visible = true;
-            
-            label2.Text = "Loading the file...";
+            //label1.Visible = true;
+
+            //label2.Text = "Loading the file...";
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 choosenFile = openFileDialog1.FileName;
-                DialogResult _dg = MessageBox.Show(
+                export();
+            }
+            /*
+                /DialogResult _dg = MessageBox.Show(
                 "Ａｌｓｏ　ａｐｐｅｎｄ　ｔｏ　ｔｅｘｔｂｏｘ？　ス現ぅ加",
                 "",
                 MessageBoxButtons.YesNo,
@@ -100,14 +103,12 @@ namespace g_l_i_t_c_h___e_r
 
                 label2.Text = "Finished...";
                 button2.Visible = true;
-
-            }
+                */
         }
-
-        private void button2_Click(object sender, EventArgs e)
+        private void export()
         {
             DirectoryInfo info = Directory.GetParent(choosenFile);
-            string parent = info.ToString()+@"\exported_1.bmp";
+            string parent = info.ToString() + @"\exported_1.bmp";
             ByteArrayToFile(parent);
         }
 
@@ -123,13 +124,14 @@ namespace g_l_i_t_c_h___e_r
             {
                 btFile[lIdx] = binReader.ReadByte();
             }
+            binReader.Close();
            // MessageBox.Show("dsds "+fileName + " "+btFile.Length );
             try
             {
+                btFile = ImageBend(btFile);
                 using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
                 {
                     fs.Write(btFile, 0, btFile.Length); //append dat shit and create the image
-                  
                 }
             }
             catch (Exception ex)
@@ -141,6 +143,45 @@ namespace g_l_i_t_c_h___e_r
         private void btn_exit_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+
+        private byte[] ImageBend(byte[] bytemap)
+        {
+            byte[] _newBytemap = bytemap;
+            Random _rnd = new Random();
+            int grab_min, grab_max;
+            grab_min = Convert.ToInt32(_newBytemap.Length / 2000);
+            grab_max = Convert.ToInt32(_newBytemap.Length / 500);
+
+            for (int i = 0; i < 10; i++)
+            {
+                int grab;
+                int grab_index;
+                grab_index = _rnd.Next(820, bytemap.Length - grab_max);
+                grab = _rnd.Next(_rnd.Next(1, grab_min), _rnd.Next(grab_min + 1, grab_max));
+                List<byte> _tmp = new List<byte>();
+                for (int j = grab_index; j < grab_index + grab - 1; j++)
+                    _tmp.Add(bytemap[j]);
+
+                int insert;
+                do
+                {
+                    insert = _rnd.Next(820, bytemap.Length - grab_max);
+                } while (insert == grab_index);
+
+                for (int k = insert; k < insert + grab - 1; k++)
+                {
+                    _newBytemap[k] = _tmp[0];
+                    _tmp.RemoveAt(0);
+                }
+            }
+            return _newBytemap;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
